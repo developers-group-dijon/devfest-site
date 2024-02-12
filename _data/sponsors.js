186e710290292aa7952b672c8e8ba24a90307396
Xavier Calland <xca@atolcd.com>
2024-02-12 09:32:39 +0100

    data: ajoute les infos sponsors 2023

--- _data/sponsors.js
+++ _data/sponsors.js
@@ -13,9 +13,44 @@ const sponsors = [
     name: "Sponsors",
     sponsors: [
       {
+        name: "AER BFC",
+        website: "https://aer-bfc.com",
+        logoUrl: "/sponsors/aer-bfc.png",
+      },
+      {
+        name: "BeBoost",
+        website: "https://www.beboost.com",
+        logoUrl: "/sponsors/beboost.png",
+      },
+      {
+        name: "Cadoles",
+        website: "https://www.cadoles.com",
+        logoUrl: "/sponsors/cadoles.png",
+      },
+      {
+        name: "Dijon Métropole",
+        website: "https://www.dijon-metropole.fr",
+        logoUrl: "/sponsors/dijon-metropole.png",
+      },
+      {
+        name: "French tech BFC",
+        website: "https://lafrenchtechbfc.fr/",
+        logoUrl: "/sponsors/french-tech-bfc.png",
+      },
+      {
         name: "Google",
         website: "https://www.google.com",
         logoUrl: "/sponsors/google.png",
+      },
+      {
+        name: "La poste",
+        website: "https://www.laposte.fr",
+        logoUrl: "/sponsors/la-poste.png",
+      },
+      {
+        name: "Odalid",
+        website: "https://odalid.com",
+        logoUrl: "/sponsors/odalid.png",
       },
     ],
   },
@@ -23,9 +58,44 @@ const sponsors = [
     name: "Village entreprise",
     sponsors: [
       {
-        name: "Google",
-        website: "https://www.google.com",
-        logoUrl: "/sponsors/google.png",
+        name: "Alteca",
+        website: "https://www.alteca.fr",
+        logoUrl: "/sponsors/alteca.png",
+      },
+      {
+        name: "Atol CD",
+        website: "https://www.atolcd.com",
+        logoUrl: "/sponsors/atolcd.png",
+      },
+      {
+        name: "Cadoles",
+        website: "https://www.cadoles.com",
+        logoUrl: "/sponsors/cadoles.png",
+      },
+      {
+        name: "Carsat BFC",
+        website: "https://www.carsat-bfc.fr/",
+        logoUrl: "/sponsors/carsat.png",
+      },
+      {
+        name: "CPage",
+        website: "https://www.cpage.fr/",
+        logoUrl: "/sponsors/cpage.png",
+      },
+      {
+        name: "Inetum",
+        website: "https://www.inetum.com",
+        logoUrl: "/sponsors/inetum.png",
+      },
+      {
+        name: "Planet Bourgogne",
+        website: "https://www.planetb.fr",
+        logoUrl: "/sponsors/planet-bourgogne.png",
+      },
+      {
+        name: "YMAG",
+        website: "https://www.ymag.fr",
+        logoUrl: "/sponsors/ymag.png",
       },
     ],
   },
