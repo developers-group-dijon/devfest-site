1a511a8534d8162bf1e22661aa83eea967f8fc09
Xavier Calland <xca@atolcd.com>
2024-02-12 09:06:31 +0100

    build: ajout une type data à commit lint
    pour identifier les modifications concernant les données des événements

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,24 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      // On utilise la liste de config-conventional et on ajoute 'data'
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "data",
+      ],
+    ],
+  },
 };
