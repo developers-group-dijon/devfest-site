98b85bae93969cc57b9e6b0cd79622e6ffaf8f88
Xavier Calland <xca@atolcd.com>
2025-06-13 14:41:02 +0200

    ci: améliore le process de check
    ajoute eslint-plugin-depend
    remplace eslint-plugin-compat par @eslint/compat

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
 import js from "@eslint/js";
-import compat from "eslint-plugin-compat";
+import { includeIgnoreFile } from "@eslint/compat";
+import depend from "eslint-plugin-depend";
 import jsdoc from "eslint-plugin-jsdoc";
 import globals from "globals";
+import { fileURLToPath } from "node:url";
 
 export default [
+  includeIgnoreFile(fileURLToPath(new URL(".gitignore", import.meta.url))),
   js.configs.recommended,
   jsdoc.configs["flat/recommended"],
-  compat.configs["flat/recommended"],
+  depend.configs["flat/recommended"],
   {
     rules: {
       "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
