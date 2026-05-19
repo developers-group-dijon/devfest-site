37ec591f312cdff3e15be3a24b58941168afcab7
Xavier Calland <x.calland@gmail.com>
2026-05-19 11:21:42 +0200

    data: ajoute les données 2025 pour tester les nouvelles fonctionnalités

--- _data/tracks.js
+++ _data/tracks.js
@@ -10,17 +10,21 @@ export default [
   {
     id: "niepce",
     name: "Amphi. Niepce",
+    sponsor: "Infotel",
   },
   {
     id: "recoura",
     name: "Amphi. Recoura",
+    sponsor: "Atol Conseils & Développments",
   },
   {
     id: "paris",
     name: "Amphi. Paris",
+    sponsor: "BPCE Solutions Informatiques",
   },
   {
     id: "bernard",
     name: "Amphi. Bernard",
+    sponsor: "DIIAGE",
   },
 ];
