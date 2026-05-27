fb70e620641efd270c4a566e9f747cadc7bcf88f
Xavier Calland <x.calland@gmail.com>
2026-05-27 13:13:12 +0200

    style: met à jour le stype pour 2026

--- .eleventy.js
+++ .eleventy.js
@@ -22,25 +22,37 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     _assets: "/assets",
   });
-  // Sous-ensemble Poppins : seuls les poids effectivement utilisés en CSS
+  // Sous-ensemble Noto Sans : seuls les poids effectivement utilisés en CSS
   // (200,300,400,500,600) + 700 pour le <strong> par défaut du Markdown
   // + 400 italic pour le <em>. Les autres @font-face restent déclarés
   // dans latin.css mais ne sont jamais demandés par le navigateur faute
   // de règle qui les référence.
-  const poppinsFiles = {
-    "./node_modules/@fontsource/poppins/latin.css":
-      "./assets/poppins/latin.css",
-    "./node_modules/@fontsource/poppins/latin-italic.css":
-      "./assets/poppins/latin-italic.css",
-    "./node_modules/@fontsource/poppins/files/poppins-latin-400-italic.woff2":
-      "./assets/poppins/files/poppins-latin-400-italic.woff2",
+  const notoSansFiles = {
+    "./node_modules/@fontsource/noto-sans/latin.css":
+      "./assets/noto-sans/latin.css",
+    "./node_modules/@fontsource/noto-sans/latin-italic.css":
+      "./assets/noto-sans/latin-italic.css",
+    "./node_modules/@fontsource/noto-sans/files/noto-sans-latin-400-italic.woff2":
+      "./assets/noto-sans/files/noto-sans-latin-400-italic.woff2",
   };
   for (const weight of [200, 300, 400, 500, 600, 700]) {
-    const name = `poppins-latin-${weight}-normal.woff2`;
-    poppinsFiles[`./node_modules/@fontsource/poppins/files/${name}`] =
-      `./assets/poppins/files/${name}`;
+    const name = `noto-sans-latin-${weight}-normal.woff2`;
+    notoSansFiles[`./node_modules/@fontsource/noto-sans/files/${name}`] =
+      `./assets/noto-sans/files/${name}`;
   }
-  eleventyConfig.addPassthroughCopy(poppinsFiles);
+  eleventyConfig.addPassthroughCopy(notoSansFiles);
+
+  // Bitcount Grid Single : police display "pixel" variable pour certains titres
+  // de l'accueil. On charge le fichier "full" (tous les axes : wght, slnt, CRSV,
+  // ELSH, ELXP) pour permettre font-variation-settings. Seul le sous-ensemble
+  // latin est copié ; le @font-face latin-ext de full.css n'est jamais demandé
+  // (texte des titres en latin), donc son woff2 n'a pas besoin d'être copié.
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/@fontsource-variable/bitcount-grid-single/full.css":
+      "./assets/bitcount-grid-single/full.css",
+    "./node_modules/@fontsource-variable/bitcount-grid-single/files/bitcount-grid-single-latin-full-normal.woff2":
+      "./assets/bitcount-grid-single/files/bitcount-grid-single-latin-full-normal.woff2",
+  });
 
   ////
   // Filtres
